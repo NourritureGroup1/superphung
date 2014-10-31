dda80d70f83134ced43851e014fff98ea6b4527d
superphung <superphung44@gmail.com>
2014-10-31 21:36:02 +0800

    remove hello.js and helloHandler.js

--- routes/hello.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Eric on 28/10/2014.
- */
-var hello = require("./helloHandler")
-
-module.exports = function (app) {
-    app.get("/hello", hello.home);
-};
--- routes/helloHandler.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Eric on 28/10/2014.
- */
-
-exports.home = function (req, res) {
-    res.end("Hello world");
-};
