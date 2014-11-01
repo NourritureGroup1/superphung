1a3e04905f604547d70a125cda37b2ce71b52b95
superphung <superphung44@gmail.com>
2014-11-01 15:40:46 +0800

    debut d'implementation de l'authentification de user et chiffrage

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
 var express = require("express");
 var https = require("https");
 var fs = require("fs");
+var formidable = require("formidable");
+var sjcl = require("sjcl");
 var app = express();
+
 //module.exports = app;
 
 var mongoose = require("mongoose");
@@ -17,10 +20,25 @@ var options = {
 
 function main () {
 
+    app.use(express.static(__dirname + "/public"));
     require("./config")(app);
 
     app.get("/", function(req, res) {
-       res.end("Welcome to the dashboard" + " " + req.protocol);
+       //res.end("Welcome to the dashboard" + " " + req.protocol);
+        //res.render("index");
+        res.sendFile(__dirname + "/public/index.html");
+    });
+
+    app.post("/data", function(req, res) {
+        var form = new formidable.IncomingForm();
+
+        form.parse(req, function(err, fields, files) {
+            console.log(fields);
+            var pass = sjcl.decrypt("pass", fields.pass);
+            console.log(pass);
+            res.end("va te faire enculer par des grosses vaches");
+
+        });
     });
 
     require("./routes")(app);
--- config/index.js
+++ config/index.js
@@ -7,8 +7,9 @@ var bodyParser = require("body-parser");
 module.exports = function(app) {
     app.set("title", "Nourriture");
     app.set("port", process.env.PORT || 8081);
-    app.set("views", __dirname + "/views");
+    //app.set("views", __dirname + "/views");
     app.set("models", __dirname + "/models");
+    app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 };
